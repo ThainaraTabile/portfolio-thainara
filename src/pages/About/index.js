18936a52ca43d5116247ba91908812ed211d6a51
Thainara Tabile <thainara.tabilebenevides@gmail.com>
2023-08-08 11:40:50 -0300

    add: new visual elements

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -5,41 +5,41 @@ export default function About() {
   return (
     <div className={styles.containers}>
       <article>
-           <div className={styles.lineNumbers}>
-           <pre>
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-      </pre>
-     </div>
-      <div class={styles.codeContent}>
-        <p>Em um mundo onde pixels se transformam em possibilidades, minha jornada como desenvolvedora web front-end é impulsionada </p>
+        <div className={styles.lineNumbers}>
+          <pre>
+            18
+            19
+            20
+            21
+            22
+            23
+            24
+            25
+            26
+            27
+            28
+            29
+            30
+            31
+            32
+            33
+            34
+            35
+            36
+            37
+            38
+            39
+            40
+          </pre>
+        </div>
+        <div class={styles.codeContent}>
+          <p>Em um mundo onde pixels se transformam em possibilidades, minha jornada como desenvolvedora web front-end é impulsionada </p>
           <p>pelo desejo de traduzir ideias em interfaces interativas, responsivas e funcionais.</p>
-       <p> Minha abordagem  é guiada pela combinação de criatividade, atenção aos detalhes,  desenvolvimento de estruturas claras e legíveis.</p>
-        <p>direcionar minha trajetória profissional para o mundo do desenvolvimento.</p>
-      </div>
+          <p> Minha abordagem  é guiada pela combinação de criatividade, atenção aos detalhes,  desenvolvimento de estruturas claras e legíveis.</p>
+          <p>direcionar minha trajetória profissional para o mundo do desenvolvimento.</p>
+        </div>
       </article>
-      <Tools/>
+      <Tools />
     </div>
 
   )
